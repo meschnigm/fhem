3c0919b626802777b7bb83b427a6cffd0dab4477
meschnigm <manuel.meschnig@web.de>
2021-01-09 18:18:37 +0100

    png. Files added to control.txt

--- prepare_update.sh
+++ prepare_update.sh
@@ -9,4 +9,4 @@ do
     FILESIZE=$(stat -c%s "$FILE")
         FILE=$(echo "$FILE"  | cut -c 3-)
         printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls_webserver.txt
-done <   <(find ./ -maxdepth 4 \( -name "*.cfg" -o -name "*.html" -o -name "SVG_ESS*" \) -print0 | sort -z -g)
+done <   <(find ./ -maxdepth 4 \( -name "*.cfg" -o -name "*.html" -o -name "*.png" -o -name "SVG_ESS*" \) -print0 | sort -z -g)
