058274c87dce36e3de668bf01c110dc62dc7b204
meschnigm <manuel.meschnig@web.de>
2021-01-16 14:31:11 +0100

    Bugfix FHEM/99_myUtils.pm wurde nicht verteilt

--- prepare_update.sh
+++ prepare_update.sh
@@ -12,5 +12,5 @@ do
             FILE=$(echo "$FILE"  | cut -c 3-)
             printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls_webserver.txt
     fi 
-done <   <(find ./ -maxdepth 4 \( -name "*.cfg" -o -name "*.html" -o -name "*.png" -o -name "SVG_ESS*" \) -print0 | \
+done <   <(find ./ -maxdepth 4 \( -name "*.cfg" -o -name "*.html" -o -name "*.png" -o -name "SVG_ESS*" -o -name "99*" \) -print0 | \
  sort -z -g)
