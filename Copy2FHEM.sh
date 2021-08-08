b12c468258c7c7b14ad6e1f5891e6c293e6ebea9
meschnigm <manuel.meschnig@web.de>
2021-08-08 12:29:10 +0200

    split BO and CS_Steuerung

--- Copy2FHEM.sh
+++ Copy2FHEM.sh
@@ -18,6 +18,9 @@ then
     cp /opt/fhem/FHEM/99_myUtils.pm      /home/pi/Git-Clones/fhem/FHEM
     cp /opt/fhem/FHEM/99_myUtils_CS_Steuerung.pm      /home/pi/Git-Clones/fhem/FHEM
     cp /opt/fhem/FHEM/99_myUtils_BusinessOptimum.pm      /home/pi/Git-Clones/fhem/FHEM
+    cp /opt/fhem/FHEM/00_CS_Steuerung.cfg  /home/pi/Git-Clones/fhem/FHEM
+    cp /opt/fhem/FHEM/00_BusinessOptimum.cfg /home/pi/Git-Clones/fhem/FHEM
+
 
    #cp /opt/fhem/FHEM/00_Private.cfg     /home/pi/Git-Clones/fhem/FHEM
    
